8e26bc9f8c1b9f853a1b3207a65f9df993dbc11a
Phodal HUANG <h@phodal.com>
2019-01-30 07:33:30 +0800

    feat: add some mode

--- src/js/simplemde.js
+++ src/js/simplemde.js
@@ -11,6 +11,8 @@ require("codemirror/mode/cmake/cmake.js");
 require("codemirror/mode/gfm/gfm.js");
 require("codemirror/mode/javascript/javascript.js");
 require("codemirror/mode/css/css.js");
+require("codemirror/mode/xml/xml.js");
+require("codemirror/mode/yaml/yaml.js");
 
 require("codemirror/addon/mode/overlay.js");
 require("codemirror/addon/display/fullscreen.js");
