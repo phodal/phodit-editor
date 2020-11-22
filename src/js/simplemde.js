84dd229260bf005c8c63b87f1a53dd67572dbec4
Phodal Huang <h@phodal.com>
2020-11-22 21:21:21 +0800

    build: add more languages

--- src/js/simplemde.js
+++ src/js/simplemde.js
@@ -13,9 +13,11 @@ require("codemirror/mode/javascript/javascript.js");
 require("codemirror/mode/css/css.js");
 require("codemirror/mode/xml/xml.js");
 require("codemirror/mode/yaml/yaml.js");
+require("codemirror/mode/toml/toml.js");
 require("codemirror/mode/rust/rust.js");
 require("codemirror/mode/go/go.js");
 require("codemirror/mode/groovy/groovy.js");
+require("codemirror/mode/python/python.js");
 
 require("codemirror/addon/mode/overlay.js");
 require("codemirror/addon/display/fullscreen.js");
